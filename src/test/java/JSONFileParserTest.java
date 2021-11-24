5fb3421ef8a4466f16a6bb2a3880cd661ad52906
andreigp <unityworldonline@gmail.com>
2021-11-24 03:50:37 +0300

    Add Spring Core, Spring Context and Spring Bean dependencies Add applicationsContext.xml Add Lombok dependency

---
+++ src/test/java/JSONFileParserTest.java
@@ -0,0 +1,21 @@
+import com.delsystem.instamart.util.JSONFileParser;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.file.NoSuchFileException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * deliverysystem Created by Home Work Studio AndrHey [andreigp]
+ * FileName: JSONFileParserTest.java
+ * Date/time: 23 ноябрь 2021 in 20:09
+ */
+
+public class JSONFileParserTest {
+    @Test(expected = NoSuchFileException.class)
+    public void getCurrentOrdersMapShouldThrowsNoSuchFileException() {
+        new JSONFileParser("qwerty").getCurrentOrdersMap();
+    }
+}
